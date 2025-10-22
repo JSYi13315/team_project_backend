eb18987085e0fc54462b9875327e12dcf61e4cb6
Hanna-Wu18 <wu.kasome@gmail.com>
2025-10-22 23:13:52 +0800

    add-auth.middleware.ts

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'can't find Token' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // pastikan decoded punya isMember
+    next();
+  } catch {
+    res.status(401).json({ error: 'Token not valid' });
+  }
+};
